ec9e2bdcc9d1e95805386856ba472a433c8286cf
Mariamelloo <105370969+Mariamelloo@users.noreply.github.com>
2023-08-16 09:46:26 -0300

    Add files via upload

--- contato.go
+++ contato.go
@@ -1,21 +1,37 @@
 package main
 
-//"bufio"
-//"os"
+import "fmt"
+
 
 type Contato struct {
 	Nome  string
 	Email string
 }
 
-func (c Contato) Altera(novoEmail string) {
+func (c * Contato) Altera(novoEmail string){
 	c.Email = novoEmail
 }
 
-func main() {
+func adicionaContato(contato, a [4]Contato) [4]Contato{
+	contatos[0] = c
+	for _, c := range [4]Contato{
+		if c == ""{
+			contatos[] = contato
+			break
+		}
+	}
+}
+
+func main(){
 	contato1 := Contato{
-		Nome:  "Maria",
-		Email: "mariamello@gmail.com",
+		Nome:	"Maria",
+		Email:	"mariamello@gmail.com",
 	}
 
+	fmt.Println(contato1)
+
+	var contatos [4]Contato
+	fmt.Println(contatos)
+
 }
+
